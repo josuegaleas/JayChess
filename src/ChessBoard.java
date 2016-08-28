b61ae74320817dd4cbd26f9cef3b5925ea0e59b0
josuegaleas <josuejgaleas@gmail.com>
2016-08-27 19:59:56 -0700

    Continued working on chessboard GUI; it nows returns location of each square

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,20 +1,20 @@
 /*
  * Author: Josue Galeas
- * Last Edit: August 26th, 2016
+ * Last Edit: August 27th, 2016
  */
 
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import javax.swing.BorderFactory;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class ChessBoard extends JPanel
 {
 	private ChessPanel[][] panels = new ChessPanel[8][8];
-	private Color light = new Color(250, 210, 160);
-	private Color dark = new Color(210, 140, 70);
 
 	public ChessBoard()
 	{
@@ -26,35 +26,58 @@ public class ChessBoard extends JPanel
 		{
 			for (int y = 0; y < 8; y++)
 			{
-				panels[x][y] = new ChessPanel();
-
-				if (x % 2 == 0)
-				{
-					if (y % 2 == 0)
-						panels[x][y].setBackground(light);
-					else
-						panels[x][y].setBackground(dark);
-				}
-				else
-				{
-					if (y % 2 != 0)
-						panels[x][y].setBackground(light);
-					else
-						panels[x][y].setBackground(dark);
-				}
-
+				panels[x][y] = new ChessPanel(x, y);
 				add(panels[x][y]);
 			}
 		}
 	}
 
-	private class ChessPanel extends JPanel
+	private class ChessPanel extends JPanel implements MouseListener
 	{
-		// TODO: Expand this panel to contain pieces
+		private int x;
+		private int y;
 
-		public ChessPanel()
+		public ChessPanel(int x, int y)
 		{
-			// TODO
+			this.x = x;
+			this.y = y;
+			addMouseListener(this);
+
+			Color light = new Color(250, 210, 160);
+			Color dark = new Color(210, 140, 70);
+
+			if (x % 2 == 0)
+			{
+				if (y % 2 == 0)
+					setBackground(light);
+				else
+					setBackground(dark);
+			}
+			else
+			{
+				if (y % 2 != 0)
+					setBackground(light);
+				else
+					setBackground(dark);
+			}
 		}
+
+		@Override
+		public void mouseClicked(MouseEvent e)
+		{
+			System.out.printf("There was a click at (%d, %d)!\n", x, y);
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {}
+
+		@Override
+		public void mouseExited(MouseEvent e) {}
 	}
 }
