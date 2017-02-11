6a17c3afaaea68537d49e441a2fcf894c86b6428
josue_osx <jdevalerie@gmail.com>
2017-02-11 13:39:48 -0800

    Reworked Board, added Move class

--- src/Board.h
+++ src/Board.h
@@ -1,25 +1,26 @@
 /*
  * Author: Josue Galeas
- * Last Edit: Feb 6, 2017
+ * Last Edit: Feb 9, 2017
  */
 
 #ifndef BOARD_H
 #define BOARD_H
 
 #include "Piece.h"
-/* #include "ActivePiece.h" */
 
 class Board
 {
 	private:
-		Piece board[8][8];
+		Piece *board;
 		void setupPieces(int);
 		void setupSymbols();
 
 	public:
 		Board();
-		/* ActivePiece *getActivePiece(int *); */
-		Piece *getPiece(int, int);
+		~Board();
+		Piece *getBoard() const {return board;};
+		Piece *getPiece(int, int) const;
+		Piece *getPiece(int *) const;
 		void printBoard() const;
 		void drawBoard() const;
 };
