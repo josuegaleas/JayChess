f91de0aca63ff25eb33d2771cf3c5ed03c7fb5f4
josuegaleas <josuejgaleas@gmail.com>
2017-08-28 01:18:43 -0700

    2017.08.28 Update
    - Added new set functions to Piece
    - Started some of the movement code
    - Updated rules in makefile to add in new verification code
    - Readme updated and to do list added

---
+++ src/Movement.hpp
@@ -0,0 +1,14 @@
+/*
+ * Author: Josue Galeas
+ * Last Edit: August 28, 2017
+ */
+
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
+
+#include "Move.hpp"
+#include "Board.hpp"
+
+void updatePieces(Move *, Board *);
+
+#endif
